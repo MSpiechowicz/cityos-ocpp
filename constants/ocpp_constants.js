567653fd7fa723a4d5fdb0a35bed818c6126c959
Maciej Spiechowicz <maciej.spiechowicz@gmail.com>
2020-03-24 21:18:24 +0100

    Changing multiline message type

--- constants/ocpp_constants.js
+++ constants/ocpp_constants.js
@@ -1,6 +1,4 @@
-/*
-* Current RC for OCPP is set to version 2.0.1.
-*/
+// Current RC for OCPP is set to version 2.0.1.
 
 export const VERSION_1_6 = 'ocpp1.6';
 export const VERSION_2_0 = 'ocpp2.0';
