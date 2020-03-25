527cc26a6e18b0fe25740810aa5cf93d4df47211
Maciej Spiechowicz <maciej.spiechowicz@gmail.com>
2020-03-25 17:42:13 +0100

    Adding ClearedChargingLimitCommand

---
+++ enums/charging_limit_source.js
@@ -0,0 +1,22 @@
+// Enumeration for indicating from which source a charging limit originates.
+
+// ChargingLimitSourceEnumType is used by: 
+// notifyChargingLimit:NotifyChargingLimitRequest.ChargingLimitType
+// clearedChargingLimit:ClearedChargingLimitRequest
+// getChargingProfiles:GetChargingProfilesRequest.ChargingProfileCriterionType
+// reportChargingProfiles:ReportChargingProfilesRequest
+
+// Description
+export const VALUE = 'Value';
+
+// Indicates that an Energy Management System has sent a charging limit
+export const EMS = 'EMS';
+
+// Indicates that an external source, not being an EMS or system operator, has sent a charging limit
+export const OTHER = 'Other';
+
+// Indicates that a System Operator (DSO or TSO) has sent a charging limit
+export const SO = 'SO';
+
+// Indicates that the CSO has set this charging profile
+export const CSO = 'CSO';
