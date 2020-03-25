87568c79b998813a9a5b48e25f1aa7783c8d3132
Maciej Spiechowicz <maciej.spiechowicz@gmail.com>
2020-03-25 17:48:07 +0100

    Adding CustomerInformationCommand

---
+++ commands/customer_information.js
@@ -0,0 +1,7 @@
+import Command from "./command";
+
+export default class CustomerInformationCommand extends Command {
+    constructor() {
+        super();
+    }
+}
---
+++ enums/customer_information_status.js
@@ -0,0 +1,14 @@
+// Status in CancelReservationResponse.
+// CustomerInformationStatusEnumType is used by: customerInformation:CustomerInformationResponse
+
+// Description
+export const VALUE = 'Value';
+
+// The Charging Station accepted the message
+export const ACCEPTED = 'Accepted';
+
+// When the Charging Station is in a state where it cannot process this request
+export const REJECTED = 'Rejected';
+
+// In a request to the Charging Station no reference to a customer is included
+export const INVALID = 'Invalid';
