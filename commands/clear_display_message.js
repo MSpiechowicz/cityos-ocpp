fdc2cd87e83f757cb4577764620776ef3a09ae1d
Maciej Spiechowicz <maciej.spiechowicz@gmail.com>
2020-03-25 17:32:55 +0100

    Adding ClearDisplayMessageCommand

---
+++ commands/clear_display_message.js
@@ -0,0 +1,7 @@
+import Command from "./command";
+
+export default class ClearDisplayMessageCommand extends Command {
+    constructor() {
+        super();
+    }
+}
---
+++ enums/clear_message_status.js
@@ -0,0 +1,11 @@
+// Result for a ClearDisplayMessageRequest as used in a ClearDisplayMessageResponse.
+// ClearMessageStatusEnumType is used by: clearDisplayMessage:ClearDisplayMessageResponse
+
+// Description
+export const VALUE = 'Value';
+
+// Request successfully executed: message cleared
+export const ACCEPTED = 'Accepted';
+
+// Given message (based on the id) not known
+export const UNKNOWN = 'Unknown';
