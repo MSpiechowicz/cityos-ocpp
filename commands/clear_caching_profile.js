b419b5571b0b6780ea65e3d71e2784c85ebcbfd3
Maciej Spiechowicz <maciej.spiechowicz@gmail.com>
2020-03-25 17:04:54 +0100

    Adding ClearCachingProfileCommand

---
+++ commands/clear_caching_profile.js
@@ -0,0 +1,7 @@
+import Command from "./command";
+
+export default class ClearCachingProfileCommand extends Command {
+    constructor() {
+        super();
+    }
+}
---
+++ enums/clear_caching_profile_status.js
@@ -0,0 +1,11 @@
+// Status returned in response to ClearChargingProfileRequest.
+// ClearChargingProfileStatusEnumType is used by: clearChargingProfile:ClearChargingProfileResponse
+
+// Description
+export const VALUE = 'Value';
+
+// Request has been accepted and will be executed
+export const ACCEPTED = 'Accepted';
+
+// No Charging Profile(s) were found matching the request
+export const UNKNOWN = 'Unknown';
