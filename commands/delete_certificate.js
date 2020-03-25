212ef7f838de4aab473e088acd5166e4603117ec
Maciej Spiechowicz <maciej.spiechowicz@gmail.com>
2020-03-25 17:54:07 +0100

    Adding DeleteCertificateCommand

---
+++ commands/delete_certificate.js
@@ -0,0 +1,7 @@
+import Command from "./command";
+
+export default class DeleteCertificateCommand extends Command {
+    constructor() {
+        super();
+    }
+}
---
+++ enums/delete_certificate_status.js
@@ -0,0 +1,13 @@
+// DeleteCertificateStatusEnumType is used by: deleteCertificate:DeleteCertificateResponse
+
+// Description
+export const VALUE = 'Value';
+
+// Normal successful completion (no errors)
+export const ACCEPTED = 'Accepted';
+
+// Processing failure
+export const FAILED = 'Failed';
+
+// Requested resource not found
+export const NOTFOUND = 'NotFound';
